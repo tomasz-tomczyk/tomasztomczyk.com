e5d9e1de6238a98953f24d2c61e4bfdf4e6ec8a7
Tomasz Tomczyk <me@tomasztomczyk.com>
2024-08-03 16:33:02 +0100

    Remove unused content

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,30 +6,8 @@ const blog = defineCollection({
     title: z.string(),
     description: z.string(),
     date: z.coerce.date(),
-    draft: z.boolean().optional()
-  }),
-});
-
-const work = defineCollection({
-  type: "content",
-  schema: z.object({
-    company: z.string(),
-    role: z.string(),
-    dateStart: z.coerce.date(),
-    dateEnd: z.union([z.coerce.date(), z.string()]),
-  }),
-});
-
-const projects = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    date: z.coerce.date(),
     draft: z.boolean().optional(),
-    demoURL: z.string().optional(),
-    repoURL: z.string().optional()
   }),
 });
 
-export const collections = { blog, work, projects };
+export const collections = { blog };
